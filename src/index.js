53abddc6e4a759802bf736f3821976aaa8c2b673
wilkinsonmatt <wilkinsonmatt01@gmail.com>
2022-07-04 01:02:36 -0700

    Added drop down for currency options

--- src/index.js
+++ src/index.js
@@ -4,43 +4,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeRateAPI from './API-currency-exchange.js';
 
-
-function getExchangeInfofromResponse(response,dollarAmount) {
+function getExchangeInfofromResponse(response,dollarAmount,otherCurrency) {
   if (response.conversion_rates){
-    $('.showConversion').html(response.conversion_rates. + "a dollarAmount);
+    if(otherCurrency === "JPY"){
+      $('.showConversion').html("$1.00 U.S Dollar (USD) = " + "¥" + response.conversion_rates.JPY + " Japanese Yen (JPY)" + "</br>" + "$" + dollarAmount + " U.S Dollars (USD) = " + "¥" + response.conversion_rates.JPY * dollarAmount + " Japanese Yen (JPY)");
+    } else if(otherCurrency === "EUR"){
+      $('.showConversion').html("$1.00 U.S Dollar (USD) = " + "€" + response.conversion_rates.EUR + " European Euros (EUR)" + "</br>" + "$" + dollarAmount + " U.S Dollars (USD) = " + "€" + response.conversion_rates.EUR * dollarAmount + " European Euros (EUR)");
+    } else if(otherCurrency === "GBP"){
+      $('.showConversion').html("$1.00 U.S Dollar (USD) = " + "£" + response.conversion_rates.GBP + " British Pounds (GBP)" + "</br>" + "$" + dollarAmount + " U.S Dollars (USD) = " + "£" + response.conversion_rates.GBP * dollarAmount + " British Pounds (GBP)");
+    } else if(otherCurrency === "CHF"){
+      $('.showConversion').html("$1.00 U.S Dollar (USD) = " + "₣" + response.conversion_rates.CHF + " Swiss Francs (CHF)" + "</br>" + "$" + dollarAmount + " U.S Dollars (USD) = " + "₣" + response.conversion_rates.CHF * dollarAmount + " Swiss Francs (CHF)");
+    } else if(otherCurrency === "CAD"){
+      $('.showConversion').html("$1.00 U.S Dollar (USD) = " + "$" + response.conversion_rates.CAD + " Canadian Dollars (CAD)" + "</br>" + "$" + dollarAmount + " U.S Dollars (USD) = " + "$" + response.conversion_rates.CAD * dollarAmount + " Canadian Dollars (CAD)");
+    } else {
+      $('.showConversion').html("the currency in question doesn't exist.");
+    }
   } else {
     $('.showErrors').text(`There was an error: ${response}`);
   }
 }
 
-// function getExchangeInfofromResponse(response,dollarAmount) {
-//   if (response.bikes) {
-//     $('.showTitle').html("");
-//     for(let i = 0; i < response.bikes.length; i++) { 
-//       let a = i+1;
-//       $('.showTitle').append("<b>Bike " + a + ferf + ": " + "</b>" + response.bikes[i].title + " was stolen " + "<br>");
-//     }
-//   } else {
-//     $('.showErrors').text(`There was an error: ${response}`);
-//   }
-// }
-
-async function makeApiCall(dollarAmount) {
+async function makeApiCall(dollarAmount, otherCurrency) {
   const response = await ExchangeRateAPI.getExchangeRate();
-  getExchangeInfofromResponse(response,dollarAmount);
-  console.log(response, dollarAmount);
+  getExchangeInfofromResponse(response,dollarAmount,otherCurrency);
 }
 
 
 $('#go').click(function() {
   let dollarAmount = parseInt($('#dollar-amount').val());
-  //let otherCurrency = parseInt($('#currency-type').val());
-  makeApiCall(dollarAmount);
+  let otherCurrency = $('#currency-type').val();
+  makeApiCall(dollarAmount,otherCurrency);
 });
-
-
-// Japanese Yen (JPY)
-// European Euro (EUR)
-// British Pound (GBP)
-// Swiss Franc (CHF)
-// Canadian Dollar (CAD)
